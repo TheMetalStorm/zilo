bab8989b68af907c26c9db9fb1d6dcb14a01abc5
simon <arapoglu.simon@gmx.de>
2023-08-14 00:28:57 +0200

    step 6

--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,28 @@ const std = @import("std");
 const c = @cImport({
     @cInclude("termios.h");
     @cInclude("unistd.h");
+    @cInclude("stdlib.h");
+    @cInclude("stdio.h");
 });
 
+var orig_termios: c.termios = undefined;
+const stdout = std.io.getStdOut().writer();
+
+fn disableRawMode() callconv(.C) void {
+    if (c.tcsetattr(c.STDIN_FILENO, c.TCSAFLUSH, &orig_termios) != 0) {
+        _ = c.printf("Failed to restore terminal attributes\nRestart Terminal.");
+        return;
+    }
+}
+
 fn enableRawMode() !void {
-    const stdout = std.io.getStdOut().writer();
-    var raw: c.termios = undefined;
-    if (c.tcgetattr(c.STDIN_FILENO, &raw) != 0) {
+    if (c.tcgetattr(c.STDIN_FILENO, &orig_termios) != 0) {
         try stdout.print("Failed to get terminal attributes\n", .{});
         return;
     }
+
+    _ = c.atexit(disableRawMode);
+    var raw: c.termios = orig_termios;
 
     raw.c_lflag &= @as(c_uint, c.ECHO);
     _ = c.tcsetattr(c.STDIN_FILENO, c.TCSAFLUSH, &raw);
